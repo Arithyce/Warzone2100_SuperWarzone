0e9e1456a5a2e67cc87e4255ec5dc3a849e292c1
Arithyce <108166240+Arithyce@users.noreply.github.com>
2024-05-01 20:45:09 -0400

    Update and add mod script
    Add script for Nuclear Power.

--- Super Warzone/multiplay/script/mods/superwarzone/sw.js
+++ Super Warzone/multiplay/script/mods/superwarzone/sw.js
@@ -1,1 +1,104 @@
 // Super Warzone
+
+function declarations(){
+	var nuke=0;
+	var cool=0;
+	var lCool=0;
+	var cLimit=0;
+	var lCLimit=0;
+	var nuke1=0;
+	var lNuke1=0;
+	var nuke2=0;
+	var lNuke2=0;
+	var nuke3=0;
+	var lNuke3=0;
+	var nukeUp=0;
+	var lNukeUp=0;
+	var nukeC=0;
+	var power=0;
+	var lPower=0;
+	var tPower=0;
+	var fPower=0;
+	var ticker = 0;
+	var nPrint="";
+	var cPrint="";
+	var lCPrint="";
+}
+
+const reactor_upgrades={
+	nuke1: "ReactorUpgrade01",
+	nuke2: "ReactorUpgrade02",
+	nuke3: "ReactorUpgrade03"
+}; 
+
+function eventResearched(research, structure, player){
+	if (getResearch(reactor_upgrades.nuke1).done){
+		nuke1=15;
+		lNuke1=30;
+	}
+	if (getResearch(reactor_upgrades.nuke2).done){
+		nuke2=15;
+		lNuke2=30;
+	}
+	if (getResearch(reactor_upgrades.nuke3).done){
+		nuke3=20;
+		lNuke3=40;
+	}
+	nukeUp=(nuke1+nuke2+nuke3);
+	lNukeUp=(lNuke1+lNuke2+lNuke3);
+}
+
+function consoleLog(){
+	nPrint=String(tPower);
+	cPrint=String(cLimit);
+	lCPrint=String(lCLimit);
+	console("Nuclear Power ",nPrint);
+	console("Remaining Cooling Towers ",cPrint);
+	console("Remaining Large Cooling Towers ",lCPrint);
+}
+
+function counter(){
+	ticker++1;
+	if (ticker=>120){
+		consoleLog();
+		ticker=0;
+	}
+}
+
+function countNuke(){
+	nuke=countStruct("Nuclear Reactor",selectedPlayer);
+	cool=countStruct("Cooling Tower",selectedPlayer);
+	lCool=countStruct("Large Cooling Tower",selectedPlayer);
+}
+
+function setNuke(){
+	countNuke();
+	cLimit=(nuke*4-cool);
+	lCLimit=(nuke*2-lCool);
+	if ((nuke>=1&&cool>=1)||(nuke>=1&&lCool>=1)){
+		nukeC=(nuke*(400+nukeUp+lNukeUp));
+		power=(cool*(50+nukeUp));
+		lPower=(lCool*(100+lNukeUp));
+		tPower=(power+lPower);
+		if (tPower>NukeC){
+			tpower=NukeC;
+		}
+		fPower=(tPower*powerType);
+		setPower(playerPower(selectedPlayer)+fPower);
+		counter();
+	}
+	else if (nuke==0||nuke>=1&&cool==0&&lCool==0){
+		tPower=0;
+		counter();
+	}
+}
+
+function superWarzoneLoaded(){
+	setTimer("setNuke", 5000);
+	console("Super Warzone Loaded");
+}
+
+function eventStartLevel(){
+	queue("declarations",1000);
+	queue("superWarzoneLoaded",5000);
+}
